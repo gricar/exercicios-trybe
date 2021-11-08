fe0be27c0938711cc4f9cf356f6bb8b6d3023f69
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-11-08 19:33:27 -0300

    Exercicios bonus

---
+++ fundamentos/bloco-04-Introdução-a-JavaScript-e-Lógica-de-Programação/dia-02-javascript-array-e-loop-for/bonus_03.js
@@ -0,0 +1,1 @@
+//Ex.3_Bonus - Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. 
