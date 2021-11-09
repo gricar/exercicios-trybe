6978d21990d0998e4a3c56b673acfea4461d5811
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-11-09 18:52:18 -0300

    apagado arquivos duplicado dia-3

--- fundamentos/bloco-04-Introdução-a-JavaScript-e-Lógica-de-Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/praticando-durante-a-aula/exercicio_01.js
+++
@@ -1,8 +0,0 @@
-// 1. Faça um algoritmo que calcule a soma de 1 a 1000 usando a estrutura "for" e retorne no formato:
-// A soma de 1 a 1000 é: X.
-let soma = 0;
-
-for (let i = 0; i <= 1000; i += 1) {
-    soma += i;
-}
-console.log('A soma de 1 a 1000 é:',soma)
--- fundamentos/bloco-04-Introdução-a-JavaScript-e-Lógica-de-Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/praticando-durante-a-aula/exercicios_04.js
+++
@@ -1,9 +0,0 @@
-//4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
-//Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
-const pessoa = 15;
-
-if (pessoa < 18) {
-    console.log("Pessoa menor de idade.")
-} else {
-    console.log("Pessoa maior de idade.")
-}
