1a6bb24b4fd7c3b60e9cbdced65e4d1581d859e6
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-11-17 20:56:19 -0300

    exercicio parte III

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/praticando/script-parteIII.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/praticando/script-parteIII.js
@@ -1,32 +1,20 @@
-//1. Crie um irmão para elementoOndeVoceEsta.
-const pai = document.getElementById('pai'); //busca a localização a ser inserida
-const irmao = document.createElement('section');  //tag a ser criada vazia, abaixo da ultima section class='quartoEUltimoFilho'
-irmao.id = 'irmao';          //nomeando uma id para esta nova section
-irmao.className = 'irmao';   //nomeando uma class para esta nova section
-pai.appendChild(irmao);
+//Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho .
+const pai = document.getElementById('pai');
+const paiNos = pai.childNodes;
+//console.log(paiNos)
+for (let i = paiNos.length - 1; i >= 0; i -= 1) {
+    const currentChild = paiNos[i];
+    if (currentChild.id !== 'elementoOndeVoceEsta') {
+        currentChild.remove()
+    }
+};
+
 //console.log(pai)
 
-//2. Crie um filho para elementoOndeVoceEsta.
+
 const ondeVcTa = document.getElementById('elementoOndeVoceEsta');
-const filho = document.createElement('p');
-filho.className = 'filho-paragrafo';
-filho.innerText = 'Paragrafo inserido via JS pelo exercicio 2';
-ondeVcTa.appendChild(filho);
-//console.log(ondeVcTa);
+//console.log(ondeVcTa.childNodes); //mostra ambos filho do filho
 
-//3. Crie um filho para primeiroFilhoDoFilho.
-const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
-const filhoPrimeiroFilhoDoFilho = document.createElement('section');
-filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
-primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
-//console.log(primeiroFilhoDoFilho);
-
-//4. A partir desse filho criado, acesse terceiroFilho.
-/*const one = document.getElementById('filhoPrimeiroFilhoDoFilho').parentElement.parentElement.nextElementSibling;
-console.log(one);*/
-//ou
-const terceiroFilho = filhoPrimeiroFilhoDoFilho
-      .parentElement // primeiroFilhoDoFilho
-      .parentElement // elementoOndeVoceEsta
-      .nextElementSibling; // terceiroFilho
-console.log(terceiroFilho);
+const segundoUltimoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+segundoUltimoFilho.remove();
+console.log(ondeVcTa.childNodes);
