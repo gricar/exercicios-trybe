96c9f9cdc2fb3afb627389a2e2a81c5dd441cf8c
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-12-03 20:00:39 -0300

    ex2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/course-praticando/parteII.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/course-praticando/parteII.js
@@ -12,8 +12,26 @@ const fatorial = num => {
 
 const fatorialPositivo = num => num > 1 ? fatorial(num) : 1; 
 
-console.log(fatorialPositivo(7));
+//console.log(fatorialPositivo(7));
 
 /* mesmo exercício de forma recursiva  */
 const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
 //console.log(factorial(7));
+
+
+//2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
+const longestWord = phrase => {
+  let palavrasArray = phrase.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for(word of palavrasArray){
+    if(word.length > maxLength) {
+      maxLength = word.length;
+      result = word;
+    }
+  }
+  return result;
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
