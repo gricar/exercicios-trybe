a3a07af550a36683dc0686016c50ad258bd3d7b4
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-12-28 20:58:37 -0300

    exercicio 4

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/course-praticando/my_new_project/encodeDecode.js
@@ -0,0 +1,43 @@
+// No Bloco 4 você desenvolveu um projeto para testar os seus conhecimentos em Javascript e lógica de programação: o Playground functions . Nos exercícios a seguir, você irá trabalhar com os testes para algumas funções que você criou! Aproveite para refatorá-las e usar todos os recursos que já aprendemos até aqui, como as features do Javascript ES6.
+function mapString(objectMap, string) {
+  const splitString = string.split('');
+  const mappedArray = [];
+
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+
+  return mappedArray.join('');
+}
+
+function encode(string) {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+}
+
+function decode(string) {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+}
+
+const functions = { encode, decode };
+module.exports = functions;
