bb58152870687085ee7a7261c52355fb6acfbd8a
gricar <gabriel.rodrigues.ricardo@outlook.com>
2021-12-28 20:48:10 -0300

    exercicio 2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/course-praticando/my_new_project/myRemove.js
@@ -0,0 +1,12 @@
+/* Exercício 2 Criar os testes para a função já fornecida */
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = myRemove;
