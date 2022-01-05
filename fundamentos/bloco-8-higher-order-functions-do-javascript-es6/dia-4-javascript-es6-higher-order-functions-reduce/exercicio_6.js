2517fc9b38715a88399a0fc0a022de53395ef22a
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-05 18:49:30 -0300

    exercicio 6 - nao finalizado

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio_6.js
@@ -0,0 +1,15 @@
+// 6 - Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } .
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  
+};
+
+console.log(studentAverage());
+
+const expected6 = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
