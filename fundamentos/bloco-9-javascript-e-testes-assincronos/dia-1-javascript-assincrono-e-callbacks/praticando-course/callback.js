9656f3d57429b7edaa0ecc7c64d4a63efa9f41d2
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-11 21:37:17 -0300

    para fixar - callback - exercicio 2

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/praticando-course/callback.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/praticando-course/callback.js
@@ -15,5 +15,25 @@ const getUser = (paramCallback) => { //Insira uma callback como parâmetro da fu
   return paramCallback(userToReturn);
 };
 
-console.log(getUser(userFullName)); // complete p o retorno: "Hello! My name is Ivan Ivanovich"
-console.log(getUser(userNationality)); // complete p o retorno: "Ivan is Russian"
+//console.log(getUser(userFullName)); // complete p o retorno: "Hello! My name is Ivan Ivanovich"
+//console.log(getUser(userNationality)); // complete p o retorno: "Ivan is Russian"
+
+
+//2 - Passe, como parâmetro e como retorno, uma callback para a função getUser 
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUserTwo = paramCallbk => {  //Insira uma callback como parâmetro da função getUserTwo;
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(paramCallbk(user));  //Retorne a callback passada como parâmetro na função getUser ;
+  }, delay());
+  
+};
+//Obs.: Analise o comportamento assíncrono da função getUser ao chamar getUser(userFullName) seguido de getUser(userNationality). Tem hora que o nome da pessoa é impresso antes e tem hora que a nacionalidade da pessoa é impressa antes! -> assincronicidade.
+
+getUserTwo(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUserTwo(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
