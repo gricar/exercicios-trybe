f82d1531b9d035deef00104d5144e5b8c605ebfc
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-22 11:57:30 -0300

    bloco 10 - react

---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world/testando-meu-computador/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import ComponentGabriel from './component';
+
+function App() {
+  return <ComponentGabriel />
+  } 
+
+export default App;
---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world/testando-meu-computador/src/component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ComponentGabriel extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Gabriel Rodrigues</h1>
+          <p>Estudante de Desenvolvimento Web na Trybe :)</p>
+      </div>
+    )
+  }
+}
+
+export default ComponentGabriel;
