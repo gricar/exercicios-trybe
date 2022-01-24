d10b3cf3afa677db32a16ab07802ff60869953e9
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-24 17:58:28 -0300

    refatorado o user data para ficar dinamico - map

--- Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/user-data/src/App.js
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/user-data/src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import './App.css';
 import UserProfile from './UserProfile';
+import contents from './UserContents';
 
 class App extends React.Component {
   render() {
-    const joao = {
-      id: 102,
-      name: "João",
-      email: "joao@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
-    };
-
-    const amelia = {
-      id: 77,
-      name: "Amélia",
-      email: "amelia@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
-    };
-
-    const gabriel = {
-      id: 26,
-      name: "Gabriel",
-      email: "gabriel@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2021/12/14/20/21/book-6871220_960_720.jpg"
-    };
-
     return (
       <div className="App">
-        <UserProfile user={joao} />
-        <UserProfile user={amelia} />
-        <UserProfile user={gabriel} />
+        {contents.map((item) => <UserProfile user={item} />)}
       </div>
     );
   }
