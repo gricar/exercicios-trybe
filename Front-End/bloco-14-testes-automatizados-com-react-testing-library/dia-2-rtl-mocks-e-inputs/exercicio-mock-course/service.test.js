4a0d8143b2666340cdd35591be7caf81377e06ec
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-17 19:08:14 -0300

    comentarios dos objetivos do exercicio

--- Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-mock-course/service.test.js
+++ Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-mock-course/service.test.js
@@ -1,9 +1,9 @@
 const service = require('./service');
 
 test('Validando a função "randomNumber"', () => {
-  service.randomNumber = jest.fn().mockReturnValue(10);
+  service.randomNumber = jest.fn().mockReturnValue(10);  // Utilize o mock e defina o retorno padrão como 10.
 
-  expect(service.randomNumber()).toBe(10);
-  expect(service.randomNumber).toHaveBeenCalled();
-  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber()).toBe(10);  // Teste qual seu retorno
+  expect(service.randomNumber).toHaveBeenCalled();  // Teste se a função foi chamada
+  expect(service.randomNumber).toHaveBeenCalledTimes(1); // Teste quantas vezes foi chamada
 });
