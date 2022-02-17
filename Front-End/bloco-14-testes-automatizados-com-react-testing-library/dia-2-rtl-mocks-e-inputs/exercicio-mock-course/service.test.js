d27201ef3878a0c1c26c3db3dc1222db72568759
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-17 19:16:10 -0300

    testes para a funcao com mock

--- Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-mock-course/service.test.js
+++ Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-mock-course/service.test.js
@@ -7,3 +7,13 @@ test('Validando a função "randomNumber"', () => {
   expect(service.randomNumber).toHaveBeenCalled();  // Teste se a função foi chamada
   expect(service.randomNumber).toHaveBeenCalledTimes(1); // Teste quantas vezes foi chamada
 });
+
+// 2 - Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez.
+test('testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);  // Defina o mock da função para a nova funcionalidade da função.
+
+  expect(service.randomNumber(45, 9)).toBe(5);  // Teste se a nova implementação de divisão foi aplicada
+  expect(service.randomNumber).toHaveBeenCalled();  // Teste se a nova função foi chamada
+  expect(service.randomNumber).toHaveBeenCalledTimes(1); // Teste quantas vezes foi chamada
+  expect(service.randomNumber).toHaveBeenCalledWith(45, 9);
+});
