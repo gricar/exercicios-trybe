9ba6800c4280d053cebd70790cbe9c1eb6532c88
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-12 20:16:14 -0300

    refatorado a dfuncao

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/praticando-course/hello-express/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/praticando-course/hello-express/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const app = express(); // Criar uma nova aplicação Express;
 
-app.get('/hello', handleHelloWorldRequest); //quando uma requisição com método GET for recebida no caminho /hello, a função handleHelloWorldRequest deve ser chamada;
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello World!'); // Ao tratar uma requisição com método GET no caminho /hello, enviar o status HTTP 200 que significa OK e a mensagem 'Hello World!'.
+}); //quando uma requisição com método GET for recebida no caminho /hello, a função callback é executada;
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 }); // Pedir ao Express que crie um servidor HTTP e escute por requisições na porta 3001;
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!'); // Ao tratar uma requisição com método GET no caminho /hello, enviar o status HTTP 200 que significa OK e a mensagem 'Hello World!'.
-}
 
 // node index.js
 // acessar a url http://localhost:3001/hello
