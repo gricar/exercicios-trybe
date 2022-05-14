9ad3be3f9903b7ffc74e3550514175173098c463
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-14 19:07:46 -0300

    post hello

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/agora-a-pratica/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/agora-a-pratica/index.js
@@ -1,11 +1,17 @@
-// 1) Crie uma rota GET /ping
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
+// 1) Crie uma rota GET /ping
 app.get('/ping', (_req, res) => res.json({ "message": "pong" }));
+
+// 2) Crie uma rota POST /hello -> deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+app.post('/hello', (req, res) => {
+	const { name } = req.body;
+	return res.status(201).json({ "message": `Hello, ${name}!` });
+});
 
 app.listen(3224, () => {
   console.log('Aplicação ouvindo na porta 3324');
