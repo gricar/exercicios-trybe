999e51f03bc1bebea52e2f4e148fcc1c400bd0b7
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-16 14:17:33 -0300

    token

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/agora-a-pratica/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/agora-a-pratica/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
 
 const validateProductName = require('./middlewares/validateProductName');
 const validateInfos = require('./middlewares/validateInfos');
@@ -17,7 +19,23 @@ app.post('/sales',
   validateWarrantyPeriod,
   (req, res) => {
   res.status(201).json({ message: 'Venda cadastrada com sucesso!' });
-})
+});
+
+app.post('/signup', (req, res) => {
+  try {
+    const { email, password, firstName, phone } = req.body;
+
+    if ([email, password, firstName, phone].includes(undefined)) {
+      return res.status(401).json({ message: 'missing fields' });
+    }
+
+    const token = crypto.randomBytes(8).toString('hex');
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).end();
+  }
+});
 
 app.listen(3225, () => console.log("Ouvindo na porta 3225"));
 
