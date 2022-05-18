252d4bb88f43e618b46216a28bdaea409811bebc
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-18 18:56:09 -0300

    criado rota e o modelo para retornar todos os livros

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/praticando-course/model-example/index.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/praticando-course/model-example/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.get('/authors', async (_req, res) => {
 
 	res.status(200).json(authors);
 });
+
+app.get('/books', async(req, res) => {
+  const books = await Book.getAll();
+
+  res.status(200).json(books);
+})
 
 const PORT = process.env.PORT || 3000;
 
