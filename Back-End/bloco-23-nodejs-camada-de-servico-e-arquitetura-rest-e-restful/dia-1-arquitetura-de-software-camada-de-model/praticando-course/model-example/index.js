99b08001476e1712ebad72ab22d6e8e2af810c69
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-19 20:14:41 -0300

    a

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/praticando-course/model-example/index.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/praticando-course/model-example/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const authorsRoute = require('./routes/authorsRoutes');
 const booksRoutes = require('./routes/booksRoutes');
