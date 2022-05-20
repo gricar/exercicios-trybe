50e34a493947feaf476bcdb2756ff8cf1f8b6716
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-19 21:41:30 -0300

    editar user, nao funcionou

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
@@ -1,8 +1,22 @@
 const connection = require('../db');
+const Joi = require('joi');
 
-const isValid = (firstName, lastName, email, password) => {
+const userSchema = Joi.object({
+	firstName: Joi.string().required(),
+	lastName: Joi.string().required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).required()}).messages({
+	'any.required': 'O campo {{: id="label" }} é obrigatório',
+	'string.min': 'O campo {{: id="label" }} deve ter, pelo menos, {{: id="limit" }} caracteres',
+	'string.email': 'Informe um email válido no campo {{: id="label" }}'});
+
+/* const isValid = (firstName, lastName, email, password) => {
   if (!firstName || !lastName || !email || !password) return false;
   return true;
+};
+ */
+function isValid(userData) {
+  return userSchema.validate(userData);
 };
 
 const formatUser = ({ id, first_name: firstName, last_name: lastName, email }) => {
@@ -33,7 +47,15 @@ const findById = (id)=>  {
 		
 		return null;  // caso contrário retorne nulo
 		});
-}
+};
+
+const updateUser = async (id, { firstName, lastName, email, password }) => {
+	const query = ` UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ? `;
+
+	await connection.execute(query, [firstName, lastName, email, password, id]);
+	
+	return findById(id);  // Por fim, buscamos o usuário, utilizando o método `findById` e o retornamos.
+};
 
 module.exports = {
   isValid,
@@ -41,4 +63,5 @@ module.exports = {
   create,
   findAll,
   findById,
+  updateUser,
 };
--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/routes/userRoutes.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/routes/userRoutes.js
@@ -7,5 +7,6 @@ const userRoutes = express.Router();
 userRoutes.get('/', middlewares.getAllUsers);
 userRoutes.get('/:id', middlewares.findUserById);
 userRoutes.post('/', middlewares.createUser);
+userRoutes.put('/:id', middlewares.updateUser);
 
 module.exports = userRoutes;
