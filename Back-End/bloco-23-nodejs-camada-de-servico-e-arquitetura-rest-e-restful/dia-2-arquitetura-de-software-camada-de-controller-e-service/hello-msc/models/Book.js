6e42c450db14ea2ed48bed00584a8c1db45c0729
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-22 21:19:55 -0300

    exercicio 1 - arquivos de model e service do books

---
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/models/Book.js
@@ -0,0 +1,50 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [books] = await connection.execute('SELECT * FROM model_example.books;');
+
+  return books.map(({ id, title, book_id }) => ({
+    id,
+    title,
+    bookId: book_id,
+  }));
+};
+
+const getBybookId = async (bookId) => {
+  const query = 'SELECT * FROM model_example.books WHERE book_id=?;'
+  const [books] = await connection.execute(query, [bookId]);
+
+  return books.map(({ id, title, book_id }) => ({
+    id,
+    title,
+    bookId: book_id,
+  }));
+};
+
+const getById = async (id) => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;'
+
+  const [book] = await connection.execute(query, [id]);
+
+  if (book.length === 0) return null;
+
+  return {
+    id: book.id,
+    title: book.title,
+    bookId: book.book_id,
+  };
+}
+
+const createBook = async (title, bookId) => {
+  const [book] = await connection.execute(
+    'INSERT INTO model_example.books (title, book_id) VALUES (?,?)',
+  [title, bookId],
+  );
+  return ({ id: book.insertId, title, bookId });
+};
+
+module.exports = {
+  getAll,
+  getById,
+  createBook,
+};
