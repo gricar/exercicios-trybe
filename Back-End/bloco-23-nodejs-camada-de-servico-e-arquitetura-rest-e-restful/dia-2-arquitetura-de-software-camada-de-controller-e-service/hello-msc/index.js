147814933bfcf53e6f6b6b403145b7773e43a262
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-05-22 21:16:06 -0300

    finalizado

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 
 const Author = require('./controllers/Author');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/authors', Author.getAll);
-app.get('/authors/:id', Author.findById);
-app.post('/authors', Author.createAuthor);
+app.get('/authors', rescue(Author.getAll));
+app.get('/authors/:id', rescue(Author.findById));
+app.post('/authors', rescue(Author.createAuthor));
+
+app.use(errorMiddleware);
 
 const PORT = 3232;
 
--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Authors.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Authors.js
@@ -1,56 +1,35 @@
 const Author = require('../models/Author');
 
-const getNewAuthor = (authorData) => {
-  const { id, firstName, middleName, lastName } = authorData;
-
-  const fullName = [firstName, middleName, lastName]
-    .filter((name) => name)
-    .join(' ');
-
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    name: fullName,
-  };
-};
-
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
-};
-
-const getAll = async () => {
-  const authors = await Author.getAll();
-
-  return authors.map(getNewAuthor);
-};
+const getAll = async () => Author.getAll();
 
 const findById = async (id) => {
   const author = await Author.findById(id);
 
-  if (!author) return null;
+  if (!author) {
+    return {
+      error: {
+        code: 'notFound',
+        message: `Não foi possível encontrar uma pessoa autora com o id ${id}`,
+      },
+    };
+  }
 
-  return getNewAuthor(author);
+  return author;
 };
 
 const createAuthor = async (firstName, middleName, lastName) => {
-  const validAuthor = isValid(firstName, middleName, lastName);
+  const existingAuthor = await Author.findByName(firstName, middleName, lastName);
 
-  if (!validAuthor) return false;
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Uma pessoa autora já existe com esse nome completo',
+      },
+    };
+  }
 
-  const [author] = await Author.createAuthor(firstName, middleName, lastName);
-
-  return getNewAuthor({
-    id: author.id,
-    firstName,
-    middleName,
-    lastName,
-  });
+  return Author.createAuthor(firstName, middleName, lastName);  // Caso a pessoa autora não exista e, portanto, possa ser criado
 };
 
 module.exports = {
