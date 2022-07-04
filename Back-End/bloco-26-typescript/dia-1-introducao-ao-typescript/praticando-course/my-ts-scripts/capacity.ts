07b30267f274fbcfb70e93c708564f2a3fe0d250
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-04 20:04:03 -0300

    exercicio 10

--- Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/capacity.ts
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/capacity.ts
@@ -1,3 +1,5 @@
+import readline from "readline-sync";
+
 const unitsVolume = ["km³", "hm³", "dam³", "m³", "dm³", "cm³", "mm³"];
 
 function convertVolume(value: number, fromUnit: string, toUnit: string): number {
@@ -8,4 +10,30 @@ function convertVolume(value: number, fromUnit: string, toUnit: string): number 
     return value * Math.pow(1000, exponent);
 }
 
-console.log(convertVolume(30, 'm³', 'mm³'));
+// console.log(convertVolume(30, 'm³', 'mm³'));
+
+
+function exec() {
+  // pegamos o valor a ser convertido digitado pela pessoa usuária
+  const value = readline.questionFloat("Digite o valor a ser convertido: \n");
+
+  // pedimos que a pessoa usuária escolha a unidade base
+  const fromUnitChoiceIndex = readline.keyInSelect(unitsVolume, "Escolha um número para a unidade base:");
+
+  // pedimos que a pessoa usuária escolha a unidade para conversão
+  const toUnitChoiceIndex = readline.keyInSelect(unitsVolume, "Escolha um número para a conversão:");
+
+
+  const fromUnitChoice = unitsVolume[fromUnitChoiceIndex];
+  const toUnitChoice = unitsVolume[toUnitChoiceIndex];
+
+  const result = convertVolume(value, fromUnitChoice, toUnitChoice)
+
+  // montamos a mensagem de saída
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`
+
+  // printamos a mensagem de saída no terminal
+  console.log(message);
+}
+
+exec();
