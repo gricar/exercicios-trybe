5a39ddd28da78cbfd498fb393c5955a388478976
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-04 20:13:28 -0300

    exercicio 11

--- Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/index.ts
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/index.ts
@@ -1,50 +1,16 @@
-import readline from "readline-sync";
-import Months from "./Months";
-import Seasons from "./Seasons";
+import readline from "readline-sync"; // importamos o pacote readline-sync
 
-const monthsNames = Object.values(Months);
-const choiceMonth = readline.keyInSelect(monthsNames, 'Escolha um mês do ano')
+// criamos um array de objetos com o nome do script e o caminho do script a ser executado
+const scripts = [
+    { name: "Converter comprimento", script: "./length" },
+    { name: "Converter volume", script: "./capacity" },
+    { name: "Verificar estações do ano", script: "./estacoes" }
+];
 
-// Criamos um objeto onde suas chaves são as estações
-// e seus valores são os meses. Note abaixo um segundo
-// objeto onde, sendo do hemisfério oposto, as estações
-// são opostas também
+// criamos um novo array somente com os nomes dos scripts
+const scriptNames = scripts.map(item => item.name);
 
-const seasonsSouth = {
-  [Seasons.Autumn]: [Months.MARCO, Months.ABRIL, Months.MAIO, Months.JUNHO],
-  [Seasons.Winter]: [Months.JUNHO, Months.JULHO, Months.AGOSTO, Months.SETEMBRO],
-  [Seasons.Spring]: [Months.SETEMBRO, Months.OUTUBRO, Months.NOVEMBRO, Months.DEZEMBRO],
-  [Seasons.Summer]: [Months.DEZEMBRO, Months.JANEIRO, Months.FEVEREIRO, Months.MARCO],
-}
+// pedimos à pessoa usuária para escolher um dos scripts de conversão
+const choice = readline.keyInSelect(scriptNames, "Escolha um número para executar o script de conversão de unidade");
 
-const seasonsNorth = {
-  [Seasons.Spring]: seasonsSouth[Seasons.Autumn],
-  [Seasons.Summer]: seasonsSouth[Seasons.Winter],
-  [Seasons.Autumn]: seasonsSouth[Seasons.Spring],
-  [Seasons.Winter]: seasonsSouth[Seasons.Summer],
-}
-
-const hemispheres = {
-  "Norte": seasonsNorth,
-  "Sul": seasonsSouth
-}
-
-const choiceHemisphere = readline.keyInSelect(Object.keys(hemispheres), "Escolha um hemisfério");
-
-const month = Object.values(Months)[choiceMonth];
-
-const hemisphere = Object.keys(hemispheres)[choiceHemisphere];
-
-console.log(`Mês: \n${month}`);
-console.log(`Hemisfério: \n${hemisphere}`);
-console.log(`Estações:`);
-const chosenHemisphereSeasons = Object.values(hemispheres)[choiceHemisphere];
-Object.entries(chosenHemisphereSeasons).map((entry) => {
-    const seasons = entry[0];
-    const months = entry[1];
-    // São os meses de cada estação. Caso esteja
-    // incluído em mais de uma estação, o console
-    // abaixo irá adicionar
-
-    if (months.includes(month)) console.log(seasons);
-});
+require(scripts[choice].script); // executamos o script escolhido usando o require
