4113185b868eca31006f60cca246209cff55f570
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-04 19:05:24 -0300

    praticando course

---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/calculateArea/dist/exercises.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rectangle = exports.triangle = exports.sumArray = exports.add = exports.personAge = exports.greeter = void 0;
+const greeter = (name) => `Olá ${name}`;
+exports.greeter = greeter;
+const personAge = (name, age) => `${name} tem ${age} anos!`;
+exports.personAge = personAge;
+const add = (x, y) => x + y;
+exports.add = add;
+const sumArray = (numbers) => numbers.reduce(exports.add, 0);
+exports.sumArray = sumArray;
+const triangle = (base, height) => (base * height) / 2;
+exports.triangle = triangle;
+const rectangle = (base, height) => base * height;
+exports.rectangle = rectangle;
---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/calculateArea/index.ts
@@ -0,0 +1,10 @@
+import * as Exercise from './exercises';
+
+console.log(Exercise.greeter('Maria'));
+console.log(Exercise.personAge('Maria', 40));
+console.log(`A soma do array é igual a ${Exercise.sumArray([3, 6, 9])}`);
+
+console.log(`Triângulo de base 10cm e altura 25cm: ${Exercise.triangle(10, 25)}cm²`);
+console.log(`Triângulo de base 5cm e altura 30cm: ${Exercise.triangle(5, 30)}cm²`);
+
+console.log(`Retângulo de base 7cm e altura 7cm: ${Exercise.rectangle(7, 7)}cm²`);
