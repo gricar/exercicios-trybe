a12f94c4469868c1a6673ddb105cabac95b4c4bf
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-04 19:56:43 -0300

    exercicio 5

---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/dist/length.js
@@ -0,0 +1,9 @@
+"use strict";
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+const convert = (value, fromUnit, toUnit) => {
+    const fromIndex = units.indexOf(fromUnit); // pegamos o index da unidade base no array
+    const toIndex = units.indexOf(toUnit); // pegamos o index da unidade para a conversão
+    const exponent = (toIndex - fromIndex); // calculamos o expoente a partir da diferença dos index
+    return value * Math.pow(10, exponent);
+};
+console.log(convert(50, 'cm', 'm'));
---
+++ Back-End/bloco-26-typescript/dia-1-introducao-ao-typescript/praticando-course/my-ts-scripts/length.ts
@@ -0,0 +1,11 @@
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+
+const convert = (value:number, fromUnit:string, toUnit:string):number => {
+  const fromIndex = units.indexOf(fromUnit); // pegamos o index da unidade base no array
+  const toIndex = units.indexOf(toUnit); // pegamos o index da unidade para a conversão
+  const exponent = (toIndex - fromIndex); // calculamos o expoente a partir da diferença dos index
+
+  return value * Math.pow(10, exponent);
+}
+
+console.log(convert(50, 'cm', 'm'));
