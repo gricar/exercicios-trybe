fa88ee4a90f2bd18f6dc77b12cbd9e497a881492
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-06 17:42:54 -0300

    exercicio 1

---
+++ Back-End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/index.ts
@@ -0,0 +1,7 @@
+import Car from './Car';
+import Colors from './Colors';
+
+const carroGabriel = new Car('corolla', Colors.BLACK, 7);
+
+// console.log(carroGabriel.turnOn());
+// console.log(carroGabriel.speedUp());
