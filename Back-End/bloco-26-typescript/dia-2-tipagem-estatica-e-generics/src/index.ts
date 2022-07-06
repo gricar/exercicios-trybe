af5bd77182f0f5deb385c8f8712f47aad622cda5
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-06 17:50:05 -0300

    exercicio 2

--- Back-End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/index.ts
+++ Back-End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/index.ts
@@ -1,7 +1,21 @@
 import Car from './Car';
 import Colors from './Colors';
+import Doors from './Doors';
+import Directions from './Directions';
 
-const carroGabriel = new Car('corolla', Colors.BLACK, 7);
+const carroStilo = new Car('Corolla', Colors.BLACK, 4);
 
-// console.log(carroGabriel.turnOn());
-// console.log(carroGabriel.speedUp());
+carroStilo.openTheDoor(Doors.DRIVER);
+carroStilo.closeTheDoor(Doors.DRIVER);
+carroStilo.turnOn();
+carroStilo.speedUp();
+carroStilo.speedDown();
+carroStilo.turn(Directions.LEFT);
+carroStilo.speedUp();
+carroStilo.speedDown();
+carroStilo.turn(Directions.RIGHT);
+carroStilo.stop();
+carroStilo.openTheDoor(Doors.BEHIND_DRIVER);
+carroStilo.closeTheDoor(Doors.BEHIND_DRIVER);
+
+// console.log(carroStilo.turnOn());
