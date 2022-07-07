ec5a51450bcdb6d3f050cb9096e562a832a16b3a
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-07 10:57:28 -0300

    GetAll e GetByID

---
+++ Back-End/bloco-26-typescript/dia-3-express-com-typescript/express-typescript/services/books.service.ts
@@ -0,0 +1,23 @@
+import connection from '../database/models/connection';
+import BookModel from '../database/models/book.model';
+import Book from '../interfaces/book.interface';
+
+class BookService {
+  public model: BookModel;
+
+  constructor() {
+    this.model = new BookModel(connection);
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const books = await this.model.getAll();
+    return books;
+  }
+
+  public async getById(id: number): Promise<Book> {
+    const book = await this.model.getById(id);
+    return book;
+  }
+}
+
+export default BookService;
