60ca5f48c20714ff26a7bc7407baa082ee005006
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-11 15:27:18 -0300

    feat: new Method to class

--- Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/praticando-course/para_fixar.ts
+++ Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/praticando-course/para_fixar.ts
@@ -3,7 +3,7 @@ class Tv {
   _size: number;
   _resolution: number;
   _connections: string[];
-  _connectedTo: string;
+  _connectedTo?: string;
 
   constructor(brand: string, size: number, resolution: number, connections: string[]) {
     this._brand = brand;
@@ -11,4 +11,12 @@ class Tv {
     this._resolution = resolution;
     this._connections = connections;
   }
+
+  turnOn(): void {
+    console.log(`TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \navailable connections: ${this._connections}`);
+  }
 }
+
+const samsungTv = new Tv('Samsung', 50, 1024, ['Ethernet', 'HDMI']);
+samsungTv.turnOn();
+//TV Samsung, 50", resolution: 1024, available connections: Ethernet,HDMI" 
