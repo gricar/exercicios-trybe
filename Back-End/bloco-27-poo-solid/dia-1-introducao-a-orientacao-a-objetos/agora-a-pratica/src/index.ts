fc4427dfefa41e1828ddf89802d5c8873ae0ede2
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-11 19:05:32 -0300

    exercicio 1

---
+++ Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/agora-a-pratica/src/index.ts
@@ -0,0 +1,12 @@
+import Student from "./class/Student";
+
+const personOne = new Student('202001011', 'Gabigol');
+
+// console.log(personOne);
+console.log('Nome:', personOne.name);
+
+personOne.examsGrades = [4, 7, 8, 10];
+console.log('Notas das provas: ', personOne.examsGrades);
+
+personOne.worksGrades = [8, 6.5]; //só pode ter duas notas, senão lança erro.
+console.log('Notas de trabalho', personOne.worksGrades);
