4f5985b478b11376bb9a7ea5b41c28914ef5e229
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-11 15:52:25 -0300

    definindo os atributos para private

--- Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/praticando-course/para_fixar.ts
+++ Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/praticando-course/para_fixar.ts
@@ -1,9 +1,9 @@
 class Tv {
-  _brand: string;
-  _size: number;
-  _resolution: string;
-  _connections: string[];
-  _connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
     this._brand = brand;
