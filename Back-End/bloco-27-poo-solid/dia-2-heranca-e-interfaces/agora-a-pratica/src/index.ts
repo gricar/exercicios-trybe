6921213940a6cefbb7cb85f00ccd63c7dda7f970
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-13 15:55:25 -0300

    exercicio 1 - classe Student

--- Back-End/bloco-27-poo-solid/dia-2-heranca-e-interfaces/agora-a-pratica/src/index.ts
+++ Back-End/bloco-27-poo-solid/dia-2-heranca-e-interfaces/agora-a-pratica/src/index.ts
@@ -1,5 +1,9 @@
 import Person from "./class/Person";
+import Student from "./class/Student";
 
-const gabriel = new Person('Gabriel', new Date('1996/02/16'));
+// const gabriel = new Person('Gabriel', new Date('1996/02/16'));
+const gabrielEstudante = new Student('Gabriel', new Date('1996/02/16'));
 
-console.log(gabriel);
+console.log(gabrielEstudante);
+gabrielEstudante.examsGrades = [10, 7, 5, 6];
+console.log(gabrielEstudante);
