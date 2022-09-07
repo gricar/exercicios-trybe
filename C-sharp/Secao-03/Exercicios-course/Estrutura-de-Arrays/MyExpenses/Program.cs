e3e16f627cff4a480cde6f14b4b216bed72de902
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-09-07 16:56:24 -0300

    feat: get sum of diagonal

--- C-sharp/Secao-03/Exercicios-course/Estrutura-de-Arrays/MyExpenses/Program.cs
+++ C-sharp/Secao-03/Exercicios-course/Estrutura-de-Arrays/MyExpenses/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+  public static void Main()
+  {
+    int[,] myArray = new int[3, 3] {
+      { 1, 2, 3},
+      { 4, 5, 6},
+      { 7, 8, 9}
+    };
+
+    int result = GetSumDiagonal(myArray);
+
+    Console.WriteLine(result); // 1 + 5 + 9 = 15
+  }
+
+  public static int GetSumDiagonal(int[,] matrix)
+  {
+    int sum = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum += matrix[i, i];
+    }
+
+    return sum;
+  }
+}
