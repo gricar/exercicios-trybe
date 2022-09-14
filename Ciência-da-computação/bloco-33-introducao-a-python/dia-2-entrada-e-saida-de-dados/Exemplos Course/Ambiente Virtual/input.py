8c061f5f690b73a80b2f1f6e900014f3e039164a
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-09-14 19:19:41 -0300

    note

--- Ciência-da-computação/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/Exemplos Course/Ambiente Virtual/input.py
+++ Ciência-da-computação/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/Exemplos Course/Ambiente Virtual/input.py
@@ -7,3 +7,6 @@ while guess != random_number:  # enquanto não adivinhar o número
     guess = int(input("Qual o seu palpite? "))  # pergunte a pessoa usuária um número
 
 print("O número sorteado era: ", guess)
+
+
+""" Este exercício pode rodar no terminal do vscode ou via .venv """
