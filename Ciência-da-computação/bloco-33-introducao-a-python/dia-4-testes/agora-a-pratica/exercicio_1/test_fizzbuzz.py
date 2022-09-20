71271818ceaf4cd25426a872b39a786ef0b5ffc2
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-09-20 19:54:21 -0300

    test: exercicio 1

--- Ciência-da-computação/bloco-33-introducao-a-python/dia-4-testes/agora-a-pratica/exercicio_1/test_fizzbuzz.py
+++ Ciência-da-computação/bloco-33-introducao-a-python/dia-4-testes/agora-a-pratica/exercicio_1/test_fizzbuzz.py
@@ -3,10 +3,11 @@ from fizzbuzz import fizzbuzz
 
 def test_fizzbuzz_should_return_list_of_numbers():
     assert fizzbuzz(2) == [1, 2]
+    assert fizzbuzz(3) == [1, 2, "Fizz"]
 
 
 def test_fizzbuzz_divisible_by_three_should_be_fizz():
-    assert fizzbuzz(3)[-1] == "Fizz" #pegando o último elemento [-1]
+    assert fizzbuzz(3)[-1] == "Fizz"  # pegando o último elemento [-1]
 
 
 def test_fizzbuzz_divisible_by_five_should_be_buzz():
