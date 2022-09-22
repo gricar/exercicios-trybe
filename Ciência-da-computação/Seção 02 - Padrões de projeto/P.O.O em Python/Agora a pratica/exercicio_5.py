f28b188c75b0ea243e67b2047855491269d55439
Gabriel Rodrigues Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2022-09-22 19:51:11 -0300

    fix: code smell Data Clumps, pois a repetição de credit_card nos parâmetros esperados é um indicativo que deveria ser um objeto separado.

--- Ciência-da-computação/Seção 02 - Padrões de projeto/P.O.O em Python/Agora a pratica/exercicio_5.py
+++ Ciência-da-computação/Seção 02 - Padrões de projeto/P.O.O em Python/Agora a pratica/exercicio_5.py
@@ -1,18 +1,13 @@
 class Order:
-    def __init__(
-        self,
-        items,
-        credit_card_name,
-        credit_card_number,
-        credit_card_month,
-        credit_card_year,
-        credit_card_security_code,
-    ):
+    def __init__(self, items, credit_card):
         self.items = items
-        self.credit_card_name = credit_card_name
-        self.credit_card_number = credit_card_number
-        self.credit_card_month = credit_card_month
-        self.credit_card_year = credit_card_year
-        self.credit_card_security_code = credit_card_security_code
+        self.credit_card = credit_card
 
-    # ...
+
+class CreditCard:
+    def __init__(self, name, number, month, year, security_code):
+        self.name = name
+        self.number = number
+        self.month = month
+        self.year = year
+        self.security_code = security_code
