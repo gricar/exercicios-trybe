e362c243ac2f1f4d35ab5c669660b1c4c2e87dba
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-09-23 11:59:09 -0300

    exemplo com interface

---
+++ C-sharp/Secao-04/Exercicios-Course/animal-kingdom/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace animal_kingdom;
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Wolf wolf = new();
+    Cow cow = new();
+
+    AnimalInfo(wolf);
+    AnimalInfo(cow);
+  }
+
+  public static void AnimalInfo(Animal animal)
+  {
+    Console.WriteLine($"{animal.GetType().Name} is also known as {animal.ScientificName}");
+    animal.Eat();
+  }
+}
---
+++ C-sharp/Secao-04/Exercicios-Course/animal-kingdom/Wolf.cs
@@ -0,0 +1,9 @@
+namespace animal_kingdom;
+
+public class Wolf : Animal
+{
+  public override string ScientificName => "Canis lupus";
+  public override bool CanEatMeat => true;
+  public override bool CanEatVegetables => true;
+
+}
