219c5bd0568b31cd7059751eef254c88d9c8cc42
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-09-23 10:08:53 -0300

    fix: errors flake

--- Ciência-da-computação/Seção 02 - Padrões de projeto/P.O.O em Python/Agora a pratica/exercicio_1.py
+++ Ciência-da-computação/Seção 02 - Padrões de projeto/P.O.O em Python/Agora a pratica/exercicio_1.py
@@ -5,24 +5,19 @@ class TV:
         self.__tamanho = tamanho
         self.__ligada = False
 
-    
     def aumentar_volume(self):
         if self.__volume < 99:
             self.__volume += 1
 
-    
     def diminuir_volume(self):
         if self.__volume > 0:
             self.__volume -= 1
 
-    
     def modificar_canal(self, canal):
         if canal <= 1 or canal >= 99:
             raise ValueError('Canal indisponível')
-        
+
         self.__canal = canal
 
-    
     def ligar_desligar(self):
         self.__ligada = not self.__ligada
-    
