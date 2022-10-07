492af3f258a5b7f0d054fb69c3687eeb94fc2eeb
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-10-07 14:32:53 -0300

    linq - pet example

---
+++ C-sharp/Secao-06/Exercicios Course/Pets/Pets/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+  static void Main()
+  {
+    Pet[] pets = {
+      new Pet { Name = "Zen", Age = 2 },
+      new Pet { Name = "Dino", Age = 7 },
+      new Pet { Name = "Tobby", Age = 5 },
+    };
+
+    int sumAge = pets.Sum(pet => pet.Age);
+    Console.WriteLine($"A somatóra das idades dos cachorros é {sumAge}");
+
+    int maxage = pets.Max(pet => pet.Age);
+    Console.WriteLine($"O cachorro mais velho tem {maxage} anos");
+
+    double averageAge = pets.Average(pet => pet.Age);
+    Console.WriteLine($"A média das idades dos cachorros é {averageAge:F}");
+  }
+}
