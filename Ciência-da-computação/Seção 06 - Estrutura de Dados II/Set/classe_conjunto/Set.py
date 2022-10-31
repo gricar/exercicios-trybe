80b833076f1ee0a1d6f56b3fbf8eca6f633103ad
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-10-31 14:49:59 -0300

    exemplo de estudantes

--- Ciência-da-computação/Seção 06 - Estrutura de Dados II/Set/classe_conjunto/Set.py
+++ Ciência-da-computação/Seção 06 - Estrutura de Dados II/Set/classe_conjunto/Set.py
@@ -115,3 +115,34 @@ if __name__ == "__main__":
     print(conj6.issubset(conj9))  # True
     print(conj9.issuperset(conj6))  # True
     print(conj8.issubset(conj9))  # True
+
+    print("*************************************")
+    print("Exemplo estudantes\n")
+
+    conj_estudantes = Conjunto()
+    conj_lista1 = Conjunto()
+    conj_lista2 = Conjunto()
+
+    estudantes = [1, 2, 3, 4, 5, 6, 7]
+    lista1_entregues = [1, 4, 7, 3]
+    lista2_entregues = [3, 1, 6]
+
+    for elem in estudantes:
+        conj_estudantes.add(elem)
+
+    for elem in lista1_entregues:
+        conj_lista1.add(elem)
+
+    for elem in lista2_entregues:
+        conj_lista2.add(elem)
+
+    print("Não entregaram a lista 1:", conj_estudantes.difference(conj_lista1))
+    print("Já entregaram as 2 listas:", conj_lista1.intersection(conj_lista2))
+    print(
+        "Quem já entregou pelo menos uma lista:",
+        conj_lista1.union(conj_lista2),
+    )
+    print(
+        "Quem não entregou nenhuma:",
+        conj_estudantes.difference(conj_lista1.union(conj_lista2)),
+    )
